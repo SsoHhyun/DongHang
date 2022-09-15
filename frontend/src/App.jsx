111cc114fdcc266d13ad78370e170c8c6593c401
d-perado <sjm01065@naver.com>
2022-09-15 15:35:50 +0900

    [FEAT] add spot detail

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { BrowserRouter } from "react-router-dom"
+import Navbar from "./components/navbar.jsx"
 import Router from "./router/router.jsx"
 
 function App() {
--- frontend/src/pages/createCoursePage.jsx
+++ frontend/src/pages/createCoursePage.jsx
@@ -1,7 +1,8 @@
 //여행 일정 생성 페이지
 import React from "react"
+import { Box } from "@mui/system"
 
 const CreateCoursePage = () => {
-  return <div></div>
+  return <Box></Box>
 }
 export default CreateCoursePage
