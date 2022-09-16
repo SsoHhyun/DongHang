5194426cb2b4112380835b3cf6e454b897ebd14b
minseo <chj2171@gmail.com>
2022-09-16 15:52:04 +0900

    add : survey table base and visited table

--- backend/src/main/java/com/team/pj/donghang/entity/Survey.java
+++ backend/src/main/java/com/team/pj/donghang/entity/Survey.java
@@ -1,6 +1,23 @@
 package com.team.pj.donghang.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Builder
+@ToString
 
 // 문항 선정 후 수정 예정
 public class Survey {
+    @Id
+    @OneToOne(cascade = CascadeType.ALL)
+    @MapsId
+    @JoinColumn(name = "user_no")
+    private User user;
+
 }
--- backend/src/main/java/com/team/pj/donghang/entity/Visited.java
+++ backend/src/main/java/com/team/pj/donghang/entity/Visited.java
@@ -1,6 +1,39 @@
 package com.team.pj.donghang.entity;
 
+import lombok.*;
 
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Builder
+@ToString
 // 지역 코드 관련 의논 후 수정 예정
 public class Visited {
+    @Id
+    @OneToOne(cascade = CascadeType.ALL)
+    @MapsId
+    @JoinColumn(name = "user_no")
+    private User user;
+
+    private Long Seoul;
+
+    private Long Busan;
+    private Long Daegu;
+    private Long Gwangju;
+    private Long Incheon;
+    private Long Daejeon;
+    private Long Ulsan;
+    private Long Gyeonggi;
+    private Long Gangwon;
+    private Long Chungcheongnam_do;
+    private Long Chungcheongbuk_do;
+    private Long Jeollanam_do;
+    private Long Jeollabuk_do;
+    private Long Gyeongsangnam_do;
+    private Long Gyeongsangbuk_do;
+    private Long Jeju;
 }
