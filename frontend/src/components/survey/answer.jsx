52f96316bba80af825df01d95e44fbae2bee9d09
@woosi96 <woosi96@naver.com>
2022-09-19 18:00:30 +0900

    feat: surveyPage component

---
+++ frontend/src/components/survey/answer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Box, styled, Button } from "@material-ui/core"
+
+const Answer = () => {
+  return (
+    <Box>
+      <Button>예</Button>
+      <Button>아니오</Button>
+      <Button>상관없음</Button>
+    </Box>
+  )
+}
+export default Answer
--- frontend/src/pages/surveyPage.jsx
+++ frontend/src/pages/surveyPage.jsx
@@ -1,10 +1,16 @@
-import React from "react"
+import { React, useState } from "react"
 import { Box } from "@material-ui/core"
+import Question from "../components/survey/question"
+import Answer from "../components/survey/answer"
+
+const [page, setPage] = useState(1)
 
 const SurveyPage = () => {
   return (
     <Box>
       <h1>Survey Page</h1>
+      <Question></Question>
+      <Answer></Answer>
     </Box>
   )
 }
