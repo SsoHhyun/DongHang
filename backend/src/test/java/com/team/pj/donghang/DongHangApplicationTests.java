02af0d6fcb1ee31f62373d4d778b2dfd688cead8
minseo <chj2171@gmail.com>
2022-09-20 15:04:57 +0900

    Modify : modify application test code because of jenkins build

--- backend/src/test/java/com/team/pj/donghang/DongHangApplicationTests.java
+++ backend/src/test/java/com/team/pj/donghang/DongHangApplicationTests.java
@@ -2,8 +2,8 @@ package com.team.pj.donghang;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//jenkins build 를 위하여 주석처리
+//@SpringBootTest
 class DongHangApplicationTests {
 
     @Test
