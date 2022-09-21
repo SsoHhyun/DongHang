57bded21df37a9a0b87cb5a9c98add24fa50da43
younghun <atluw@naver.com>
2022-09-21 14:52:27 +0900

    design : user information layout

--- frontend/src/components/myPage/userInfo.jsx
+++ frontend/src/components/myPage/userInfo.jsx
@@ -1,31 +1,57 @@
+import React from "react"
 import {
   Box,
   Button,
-  Paper,
   styled,
-  Tab,
-  Tabs,
   Typography,
+  Paper,
+  Input,
+  TextField,
 } from "@mui/material"
 import { fullname, username } from "react-lorem-ipsum"
-import { Photo, Name } from "../../pages/users/myPage"
+import { Photo } from "../../pages/users/myPage"
+import SmartphoneIcon from "@mui/icons-material/Smartphone"
+import EmailIcon from "@mui/icons-material/Email"
+import ChangeCircleIcon from "@mui/icons-material/ChangeCircle"
 
 const Info = () => {
+  // const ariaLabel = { "aria-label": "description" }
+
   return (
     <InfoContainer>
-      <User>
+      <MyInfo>내 정보</MyInfo>
+      <User elevation={3}>
         <UserInfo>
-          <Photo />
+          <UserPhoto>
+            <Photo />
+            <ChangePhoto
+              color="info"
+              onClick={() => {
+                console.log("hi")
+              }}
+            />
+          </UserPhoto>
           <UserName>
-            <Name>{fullname()}</Name>
+            <MyName defaultValue={fullname()}></MyName>
             <UserId>{`@${username()}`}</UserId>
           </UserName>
         </UserInfo>
-        <EditBtn>닉네임 수정</EditBtn>
+        <EditBtn>수정</EditBtn>
       </User>
-
-      <Phone></Phone>
-      <Email></Email>
+      <User elevation={3}>
+        <Phone>
+          <SmartphoneIcon />
+          <PhoneNumber defaultValue="010-0000-0000"></PhoneNumber>
+        </Phone>
+        <EditBtn>수정</EditBtn>
+      </User>
+      <User elevation={3}>
+        <Email>
+          <EmailIcon />
+          <EmailAddress defaultValue={`${username()}@gmail.com`}></EmailAddress>
+        </Email>
+        <EditBtn>수정</EditBtn>
+      </User>
     </InfoContainer>
   )
 }
@@ -41,13 +67,23 @@ const InfoContainer = styled(Box)({
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
-  alignContent: "space-evenly",
+  alignContent: "center",
 })
 
-const User = styled(Box)({
+const MyInfo = styled(Typography)({
+  fontSize: 30,
+  fontWeight: "bold",
+  textAlign: "center",
+  marginBottom: "2rem",
+})
+
+const User = styled(Paper)({
   display: "flex",
-  justifyContent: "space-around",
+  justifyContent: "space-between",
   alignItems: "center",
+  margin: "1rem",
+  borderRadius: 10,
+  backgroundColor: "ivory",
 })
 
 const UserInfo = styled(Box)({
@@ -56,11 +92,29 @@ const UserInfo = styled(Box)({
   alignItems: "center",
 })
 
+const UserPhoto = styled(Box)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "end",
+})
+
+const ChangePhoto = styled(ChangeCircleIcon)({
+  position: "absolute",
+  paddingBottom: 50,
+  paddingLeft: 150,
+})
+
 const UserName = styled(Box)({
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
-  alignItems: "center",
+})
+
+const MyName = styled(Input)({
+  color: "#c19a6b",
+  fontWeight: "bold",
+  fontSize: 25,
+  textAlign: "center",
 })
 
 const UserId = styled(Typography)({
@@ -72,6 +126,28 @@ const EditBtn = styled(Button)({
   color: "crimson",
 })
 
-const Phone = styled(Box)({})
+const Phone = styled(Box)({
+  marginLeft: "2rem",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+})
 
-const Email = styled(Box)({})
+const Email = styled(Box)({
+  marginLeft: "2rem",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+})
+
+const PhoneNumber = styled(Input)({
+  marginLeft: "1rem",
+  color: "grey",
+  fontSize: 17,
+})
+
+const EmailAddress = styled(Input)({
+  marginLeft: "1rem",
+  color: "grey",
+  fontSize: 17,
+})
