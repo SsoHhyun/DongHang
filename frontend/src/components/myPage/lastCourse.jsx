458608c477827b14cc791710e7df0c40585497a0
김영훈 <atluw@naver.com>
2022-09-22 15:54:54 +0900

    update : etc

--- frontend/src/components/myPage/lastCourse.jsx
+++ frontend/src/components/myPage/lastCourse.jsx
@@ -1,1 +1,12 @@
 // 지난 여행 코스(탭)
+import React, { useState } from "react";
+import { Box, Button, Paper, styled, Tab, Typography } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+
+const LastCourse = () => {
+  return <LastCourseContainer></LastCourseContainer>;
+};
+
+export default LastCourse;
+
+const LastCourseContainer = styled(Box)({});
