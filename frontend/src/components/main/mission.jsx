ef6c0a8176af17024fe1e985ef36174af50f04f0
@woosi96 <woosi96@naver.com>
2022-09-23 10:48:23 +0900

    feature : missionPage

--- frontend/src/components/main/mission.jsx
+++ frontend/src/components/main/mission.jsx
@@ -1,30 +1,111 @@
-import React from "react"
-import { Box, styled, Grid } from "@material-ui/core"
+import { React, useState } from "react"
+import { Box, styled, Grid, Paper, Button, IconButton } from "@material-ui/core"
+import Carousel from "react-material-ui-carousel"
+import CameraAltIcon from "@mui/icons-material/CameraAlt"
+import RefreshIcon from "@mui/icons-material/Refresh"
 
-const MissionGrid = styled(Grid)({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-})
+// const MissionGrid = styled(Grid)({
+//   display: "flex",
+//   flexDirection: "column",
+//   alignItems: "center",
+// })
 
-const MissionBox = styled(Box)({
-  background: "yellow",
-  width: "70%",
-  borderRadius: 20,
-  textAlign: "center",
-  margin: 10,
-})
+// const MissionBox = styled(Box)({
+//   background: "yellow",
+//   width: "70%",
+//   borderRadius: 20,
+//   textAlign: "center",
+//   margin: 10,
+// })
+
+// reroll button onclick -> get numbers 함수 -> 숫자 세개 나옴 ->
+// 그 숫자를 다시 인덱스로 넣어줌
+
+// mission 임시로 넣어둔 리스트
+var items = [
+  {
+    name: "Special Mission",
+    description: "부모님과 단풍잎 주워서 책갈피 만들기",
+  },
+  {
+    name: "Mission",
+    description: "부모님과 서로 가장 좋아하는 노래 소개시켜 주기",
+  },
+  {
+    name: "Mission",
+    description: "함께 눈사람 만들기",
+  },
+  {
+    name: "Mission",
+    description: "벚나무 밑에서 사진 찍기",
+  },
+  {
+    name: "Mission",
+    description: "시원한 물에 발 담그기",
+  },
+  {
+    name: "Mission",
+    description: "아이스크림 걸고 고스톱 내기",
+  },
+  {
+    name: "Mission",
+    description: "부모님 손 잡고 걷기",
+  },
+  {
+    name: "Mission",
+    description: "배에서 낚시 해보기",
+  },
+  {
+    name: "Mission",
+    description: "첨성대와 함께 찰칵",
+  },
+  {
+    name: "Mission",
+    description: "최초로 커스텀 미션 추가",
+  },
+]
 
 const Mission = () => {
   return (
-    <MissionGrid>
-      <MissionBox>부모님과 손잡고 걷기</MissionBox>
-      <MissionBox>부모님과 손잡고 걷기</MissionBox>
-      <MissionBox>부모님과 손잡고 걷기</MissionBox>
-      <MissionBox>부모님과 손잡고 걷기</MissionBox>
-      <MissionBox>부모님과 손잡고 걷기</MissionBox>
-    </MissionGrid>
+    <Box>
+      <IconButton>
+        <RefreshIcon />
+      </IconButton>
+      <Carousel background="#f4b37b">
+        {items.map((item, i) => (
+          <Item key={i} item={item} />
+        ))}
+      </Carousel>
+    </Box>
+  )
+}
+
+function Item(props) {
+  return (
+    <CarouselPaper>
+      <h2>{props.item.name}</h2>
+      <ContentBox>
+        <p>{props.item.description}</p>
+
+        <IconButton>
+          <CameraAltIcon />
+        </IconButton>
+      </ContentBox>
+    </CarouselPaper>
   )
 }
 
 export default Mission
+
+const CarouselPaper = styled(Paper)({
+  background: "#f4b37b",
+  padding: "3%",
+})
+
+const ContentBox = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: "1.2em",
+})
--- frontend/src/pages/mainPage.jsx
+++ frontend/src/pages/mainPage.jsx
@@ -5,37 +5,70 @@ import Grid from "@mui/material/Grid/Grid"
 import NowCourse from "../components/main/nowCourse"
 import Mission from "../components/main/mission"
 
-const MainBox = styled(Box)({
-  display: "flex",
-  flexDirection: "column",
-  alignContent: "center",
-  height: "100%",
-})
-
 const MainPage = () => {
   return (
-    <MainBox>
-      <h1>Main Page</h1>
+    <Background>
+      <MainBox>
+        <h1>Main Page</h1>
 
-      {/* 여행지 추천 */}
-      <Box>
-        <Grid container justifyContent="center">
-          <RecommTripCard></RecommTripCard>
-          <RecommTripCard></RecommTripCard>
-          <RecommTripCard></RecommTripCard>
-        </Grid>
-      </Box>
-
-      {/* 현재 진행중인 일정 */}
-      <Box>
-        <NowCourse></NowCourse>
-      </Box>
-
-      {/* 미션 */}
-      <Box>
-        <Mission></Mission>
-      </Box>
-    </MainBox>
+        {/* 여행지 추천 */}
+        <RecommBox>
+          <Grid container justifyContent="center">
+            <RecommTripCard></RecommTripCard>
+            <RecommTripCard></RecommTripCard>
+            <RecommTripCard></RecommTripCard>
+          </Grid>
+        </RecommBox>
+        {/* 현재 진행중인 일정 */}
+        <CourseBox>현재 진행중인 일정이 없습니다.</CourseBox>
+        {/* 미션 */}
+        <MissionBox>
+          <Mission></Mission>
+        </MissionBox>
+      </MainBox>
+    </Background>
   )
 }
 export default MainPage
+
+const Background = styled(Box)({
+  display: "flex",
+  alignContent: "center",
+  justifyContent: "center",
+  width: "100vw",
+  height: "100vh",
+  background: "#B59B89",
+})
+
+const MainBox = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "70%",
+  background: "#faf8f7",
+})
+
+const RecommBox = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  alignContent: "center",
+  width: "70%",
+})
+
+const CourseBox = styled(Box)({
+  width: "40%",
+  height: "15%",
+  border: "2px solid #322725",
+  borderRadius: "10px",
+  padding: "5%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontSize: "2em",
+  margin: "3% 0",
+})
+
+const MissionBox = styled(Box)({
+  width: "50%",
+})
