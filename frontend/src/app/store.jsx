e701415671c86082661a6bd0c090869cd25df4cf
김영훈 <atluw@naver.com>
2022-09-26 13:14:18 +0900

    add : redux setting

--- frontend/src/app/store.jsx
+++ frontend/src/app/store.jsx
@@ -1,1 +1,22 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit"
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const open = createSlice({
+  name: "open",
+  initialState: false,
+  reducers: {
+    setOpen(state) {
+      return true;
+    },
+    setClose(state) {
+      return false;
+    },
+  },
+});
+
+export let { setOpen, setClose } = open.actions;
+
+export default configureStore({
+  reducer: {
+    open: open.reducer,
+  },
+});
