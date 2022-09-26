85af225a589071aa0d176e25a71fbbe338621161
김영훈 <atluw@naver.com>
2022-09-26 09:54:39 +0900

    design : last course ing

--- frontend/src/components/myPage/lastCourse.jsx
+++ frontend/src/components/myPage/lastCourse.jsx
@@ -1,12 +1,29 @@
 // 지난 여행 코스(탭)
 import React, { useState } from "react";
-import { Box, Button, Paper, styled, Tab, Typography } from "@mui/material";
-import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Box, Button, Paper, styled, Typography } from "@mui/material";
+import Map from "../map";
 
 const LastCourse = () => {
-  return <LastCourseContainer></LastCourseContainer>;
+  return (
+    <LastCourseContainer>
+      <MapContainer id="map">
+        <Map />
+      </MapContainer>
+    </LastCourseContainer>
+  );
 };
 
 export default LastCourse;
 
-const LastCourseContainer = styled(Box)({});
+const LastCourseContainer = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItem: "center",
+});
+
+const MapContainer = styled(Map)({
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+});
