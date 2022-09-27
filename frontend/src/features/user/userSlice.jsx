f3594d0bc5ecf01ef4d103961e626534e02bc577
younghun <atluw@naver.com>
2022-09-27 10:50:18 +0900

    update : userSlice fix

--- frontend/src/features/user/userSlice.jsx
+++ frontend/src/features/user/userSlice.jsx
@@ -1,6 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit/dist/createSlice"
-import { createAsyncThunk } from "@reduxjs/toolkit/dist/createAsyncThunk"
-import { request } from "../../utils/axios"
+import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk } from "@reduxjs/toolkit"
 
 // const userData = {
 //     userId,
@@ -28,13 +27,6 @@ const userSlice = createSlice({
     },
   },
 })
-
-export const sendRegisterrequest = createAsyncThunk(
-  "sendRegisterRequest",
-  async (data) => {
-    return request("POST", "/api" + "/user/register", data)
-  }
-)
 
 const actions = userSlice
 export const fetchProfile = actions
