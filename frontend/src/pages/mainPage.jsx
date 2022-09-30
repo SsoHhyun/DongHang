3b22323bc9e7ec7a7e6dfc70f3bca2abe8f2394e
zzzso-o <1211ditto@gmail.com>
2022-09-30 20:15:09 +0900

    Feature: mainpage

--- frontend/src/pages/mainPage.jsx
+++ frontend/src/pages/mainPage.jsx
@@ -1,28 +1,46 @@
-import React from "react"
+import { React, useEffect } from "react"
+import $ from "jquery"
 import { Box, styled } from "@material-ui/core"
 import RecommTrip from "../components/main/recommTrip"
 import Grid from "@mui/material/Grid/Grid"
 import NowCourse from "../components/main/nowCourse"
 import Mission from "../components/main/mission"
-import Fade from "react-reveal/Fade" // Import react-reveal(Fade)
+// import Fade from "react-reveal/Fade"
+import { useScrollFadeIn } from "../app/hooks"
 
 const MainPage = () => {
+  // 섹션 넘어가는 애니메이션
+  window.addEventListener(
+    "wheel",
+    (e) => {
+      e.preventDefault()
+    },
+    { passive: false }
+  )
+
+  var mBackground = $("html")
+  var page = 1
+
+  mBackground.animate({ scrollTop: 0 }, 10)
+
+  $(window).on("wheel", function (e) {
+    if (mBackground.is(":animated")) return
+    if (e.originalEvent.deltaY > 0) {
+      if (page == 2) return
+      page++
+    } else if (e.originalEvent.deltaY < 0) {
+      if (page == 1) return
+      page--
+    }
+    var posTop = (page - 1) * $(window).height()
+    mBackground.animate({ scrollTop: posTop })
+  })
+
   return (
     <Background>
-      <Fade
-        bottom
-        sx={{
-          width: "100%",
-          height: "50%",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <RecomImg>
-          <Img src="img/water.jpg" alt="" />
-        </RecomImg>
-      </Fade>
-
+      <RecomImg>
+        <Img src="img/fall.jpg" alt="" />
+      </RecomImg>
       <MainBox>
         {/* 현재 진행중인 일정 */}
         <CourseBox>현재 진행중인 일정이 없습니다.</CourseBox>
@@ -38,32 +56,33 @@ export default MainPage
 
 const Background = styled(Box)({
   display: "flex",
-  flexDirection: "row",
+  flexDirection: "column",
   // justifyContent: "center",
   // alignItems: "center",
-  width: "100vw",
+  alignItems: "center",
+})
+
+const RecomImg = styled(Box)({
+  width: "100%",
   height: "100vh",
-  background: "white",
-  paddingTop: "8vh",
+  overflow: "hidden",
+})
+
+const Img = styled("img")({
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
 })
 
 const MainBox = styled(Box)({
   display: "flex",
-  flexDirection: "column",
+  flexDirection: "row",
   justifyContent: "center",
   alignItems: "center",
-  width: "50vw",
-  height: "80vh",
+  width: "80vw",
+  height: "100vh",
   background: "white",
 })
-
-// const RecommBox = styled(Box)({
-//   display: "flex",
-//   flexDirection: "column",
-//   alignContent: "center",
-//   width: "40vw",
-//   height: "100vh",
-// })
 
 const CourseBox = styled(Box)({
   width: "40%",
@@ -79,15 +98,6 @@ const CourseBox = styled(Box)({
 })
 
 const MissionBox = styled(Box)({
-  width: "50%",
-})
-
-const Img = styled("img")({
-  maxWidth: "100%",
-  height: "100%",
-})
-
-const RecomImg = styled(Box)({
-  width: "100vw",
-  height: "50vh",
+  width: "40%",
+  justifyContent: "center",
 })
