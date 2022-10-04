db151c07a88c4ba4fe9fc5ba92a7bd25a5a2b64c
황석영 <sjm01065@naver.com>
2022-10-04 16:44:53 +0900

    update

--- frontend/src/components/coursePage/courseside.jsx
+++ frontend/src/components/coursePage/courseside.jsx
@@ -11,7 +11,7 @@ import {
   DialogTitle,
 } from "@mui/material"
 import { useState } from "react"
-
+import Swal from "sweetalert2"
 import interceptor from "../../api/interceptor"
 import TextField from "@mui/material/TextField"
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
@@ -24,7 +24,7 @@ const CourseSide = (props) => {
   const [opendialog, setOpendialo] = React.useState(false)
   const [startDate, setStartDate] = React.useState(null)
   const [endDate, setEndDate] = React.useState(null)
-
+  const [courseTitle, setcouseTitle] = React.useState("")
   const parseDate = (newValue) => {
     if (newValue == null) return "0"
     let temp = newValue.$d.getFullYear().toString()
@@ -129,36 +129,49 @@ const CourseSide = (props) => {
               marginBottom: "5%",
               left: "35%",
             }}
-            onClick={handleClickOpen}
+            onClick={() => {
+              Swal.fire({
+                title: "일정의 이름을 적어주세요",
+                input: "text",
+                inputAttributes: {
+                  autocapitalize: "off",
+                },
+                showCancelButton: true,
+                confirmButtonText: "일정생성",
+                showLoaderOnConfirm: true,
+                preConfirm: (login) => {
+                  const commonNoList = []
+                  for (let i = 0; i < props.recommendspot.length; i++) {
+                    commonNoList.push(props.recommendspot[i].commonNo)
+                  }
+                  interceptor({
+                    url: "/api/trip",
+                    method: "post",
+                    data: {
+                      commonNoList: commonNoList,
+                      endDate: endDate,
+                      startDate: startDate,
+                      tripName: login,
+                    },
+                  })
+                    .then((res) => {
+                      console.log("createsuccess")
+                      Swal.fire({
+                        title: "일정이 생성되었습니다!",
+                        text: "일정이 성공적으로 생성되었습니다!",
+                        icon: "success",
+                        confirmButtonText: "확인",
+                      })
+                    })
+                    .catch((err) => {
+                      alert(err)
+                    })
+                },
+              })
+            }}
           >
             일정생성
           </Button>
-          <Dialog
-            open={opendialog}
-            onClose={handleClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-          >
-            <DialogTitle id="alert-dialog-title">{"일정생성"}</DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-                일정을 이대로 생성하시겠습니까?
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button
-                onClick={() => {
-                  handleClose()
-                  clickCreateCourse()
-                }}
-              >
-                생성
-              </Button>
-              <Button onClick={handleClose} autoFocus>
-                취소
-              </Button>
-            </DialogActions>
-          </Dialog>
         </Box>
       </Box>
     </Box>
