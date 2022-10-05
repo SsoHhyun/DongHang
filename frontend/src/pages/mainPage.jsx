288b60aa9aa2e7fdcd81c69aae32a1935f96187e
황석영 <sjm01065@naver.com>
2022-10-05 10:38:35 +0900

    update

--- frontend/src/pages/mainPage.jsx
+++ frontend/src/pages/mainPage.jsx
@@ -1,18 +1,13 @@
 import { React, useEffect } from "react"
-import $ from "jquery"
+import $, { get } from "jquery"
 import { Box, styled, Button } from "@material-ui/core"
-import RecommTrip from "../components/main/recommTrip"
-import Grid from "@mui/material/Grid/Grid"
 import NowCourse from "../components/main/nowCourse"
 import Mission from "../components/mission/mission"
-import Fade from "react-reveal/Fade"
 import AOS from "aos"
 import "aos/dist/aos.css"
 import { setUserInfo } from "../features/user/userSlice"
 import { useSelector, useDispatch } from "react-redux/es/exports"
-import SurveyOpen from "../components/main/surveyOpen"
 import { useNavigate } from "react-router-dom"
-import axios from "axios"
 import interceptor from "../api/interceptor"
 import { useState } from "react"
 import Modal from "@mui/material/Modal"
@@ -21,60 +16,37 @@ import CreateMission from "../components/mission/missionCreate"
 const MainPage = () => {
   const navigate = useNavigate()
 
-  const [myTrip, setMyTrip] = useState(0)
+  const [myTrip, setMyTrip] = useState(null)
   const [myPlace, setMyPlace] = useState([])
 
   const [open, setOpen] = useState(false)
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
 
-  // 섹션 넘어가는 애니메이션
-  // window.addEventListener(
-  //   "wheel",
-  //   (e) => {
-  //     e.preventDefault()
-  //   },
-  //   { passive: true }
-  // )
-
-  // var mBackground = $("html")
-  // var page = 1
-
-  // mBackground.animate({ scrollTop: 0.1 }, 10)
-
-  // $(window).on("wheel", function (e) {
-  //   if (mBackground.is(":animated")) return
-  //   if (e.originalEvent.deltaY > 0) {
-  //     if (page === 2) return
-  //     page++
-  //   } else if (e.originalEvent.deltaY < 0) {
-  //     if (page === 1) return
-  //     page--
-  //   }
-  //   var posTop = (page - 1) * $(window).height()
-  //   mBackground.animate({ scrollTop: posTop })
-  // })
   const dispatch = useDispatch()
 
   useEffect(() => {
     AOS.init()
-    interceptor({
-      url: "/user/info",
-      method: "get",
-    }).then((res) => {
-      dispatch(setUserInfo(res.data))
-    })
-  }, [])
-
-  useEffect(() => {
-    interceptor({
-      url: "/api/trip/getTodayTrip",
-      method: "get",
-    }).then((res) => {
-      console.log(res.data)
-      setMyTrip(res.data.tripNo)
-      setMyPlace(res.data.placeList)
-    })
+    if (localStorage.getItem("access-token")) {
+      interceptor({
+        url: "/user/info",
+        method: "get",
+      }).then((res) => {
+        dispatch(setUserInfo(res.data))
+        interceptor({
+          url: "/api/trip/getTodayTrip",
+          method: "get",
+        }).then((res) => {
+          console.log(res.data)
+          if (res.data.tripNo != null) {
+            setMyTrip(res.data.tripNo)
+          }
+          if (res.data.placeList != null) {
+            setMyPlace(res.data.placeList)
+          }
+        })
+      })
+    }
   }, [])
 
   return (
